103cc8ad5684733e88ea99bc0b46b038a6025a18
Chris Woodford <chris.woodford@gmail.com>
2014-01-24 16:53:14 -0500

    refactor api exception class and add test coverage

--- lib/Shopify/Api/Exception.php
+++ lib/Shopify/Api/Exception.php
@@ -5,22 +5,54 @@ namespace Shopify\Api;
 class Exception extends \Exception
 {
 
+    /** @var array */
     protected $info;
 
+    /**
+     * initialize the exception with the supplied info
+     * @param array $info
+     */
     public function __construct(array $info)
     {
 
         $this->info = $info;
 
         parent::__construct(
-            $info['response_headers']['http_status_message'],
-            $info['response_headers']['http_status_code']
+            $this->getResponseHeader('http_status_message'),
+            $this->getResponseHeader('http_status_code')
         );
 
     }
 
+    /**
+     * return the information originally supplied to the exception
+     * constructor
+     * @return array
+     */
     public function getInfo()
     {
-        $this->info;
+        return $this->info;
     }
+
+    /**
+     * get a response header from the info array
+     * @param string $key
+     * @return string|null
+     */
+    protected function getResponseHeader($key)
+    {
+
+        $info = $this->getInfo();
+        $headers = array_key_exists('response_headers', $info)
+            ? $info['response_headers'] : null;
+
+        if (!is_null($headers)) {
+            return array_key_exists($key, $headers)
+                ? $headers[$key] : null;
+        }
+
+        return null;
+
+    }
+
 }
