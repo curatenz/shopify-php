f4a4492d81474e00d0fad3b45e36fe0db4e56d8f
Chris Woodford <chris.woodford@gmail.com>
2014-01-25 12:18:39 -0500

    merge call limits into api client, with tests

--- tests/Shopify/Api/Tests/ClientTest.php
+++ tests/Shopify/Api/Tests/ClientTest.php
@@ -5,13 +5,18 @@ namespace Shopify\Api\Tests;
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testRequestValidation()
+    public function setUp()
     {
 
         $http = $this->getMock('Shopify\HttpClient');
+        $this->api = new \Shopify\Api\Client($http);
 
-        $api = new \Shopify\Api\Client($http);
-        $api->setSharedSecret('hush');
+    }
+
+    public function testRequestValidation()
+    {
+
+        $this->api->setSharedSecret('hush');
 
         $signature = "31b9fcfbd98a3650b8523bcc92f8c5d2";
 
@@ -22,15 +27,40 @@ class ClientTest extends \PHPUnit_Framework_TestCase
             'timestamp' => "1337178173", // 2012-05-16 14:22:53
         );
 
-        $this->assertEquals($signature, $api->generateSignature($params));
+        $this->assertEquals($signature, $this->api->generateSignature($params));
 
         $paramsWithSignature = $params;
         $paramsWithSignature['signature'] = $signature;
 
-        $this->assertTrue($api->validateSignature($paramsWithSignature));
+        $this->assertTrue($this->api->validateSignature($paramsWithSignature));
 
         // request is older than 1 day, expect false
-        $this->assertFalse($api->isValidRequest($paramsWithSignature));
+        $this->assertFalse($this->api->isValidRequest($paramsWithSignature));
+
+    }
+
+    public function testCallLimits()
+    {
+
+        $callsMade = 10;
+        $callLimit = 100;
+        $callsRemaining = $callLimit - $callsMade;
+
+        $headers = array(
+            \Shopify\Api\Client::SHOP_API_CALL_LIMIT => '10/100'
+        );
+
+        $this->assertEquals(
+            $callsMade, $this->api->getNumberOfCallsMade($headers)
+        );
+
+        $this->assertEquals(
+            $callLimit, $this->api->getCallLimit($headers)
+        );
+
+        $this->assertEquals(
+            $callsRemaining, $this->api->getNumberOfCallsRemaining($headers)
+        );
 
     }
 
